d1a7d52f951c6c2850c9368e8c448027e25abfde
cristianms1009 <litkillah1009@gmail.com>
2022-10-05 00:18:21 -0300

    se agregaron components

--- Integrador/src/app/app.module.ts
+++ Integrador/src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaLaboralComponent } from './components/experiencia-laboral/experiencia-laboral.component';
+import { HysComponent } from './components/hys/hys.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoAPComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    ExperienciaLaboralComponent,
+    HysComponent,
+    FooterComponent,
+
   ],
   imports: [
     BrowserModule,
